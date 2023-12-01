54e80d90ed55ac4928400474c9d42e5a8bc7c6f8
Shriharsh <shriharsh.dev@gmail.com>
2023-12-01 21:26:41 +0530

    Initial commit

---
+++ react-cv/src/BasicDetails.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import './App.css'
+
+const BasicDetails = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        pno: '',
+        dob: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleReset = () => {
+        setFormData({
+            name: '',
+            email: '',
+            pno: '',
+            dob: ''
+        });
+    };
+
+    return ( 
+        <div className="basicDetails">
+            <div className="personDetailForm">
+                <h1>Personal Details</h1>
+                <form onSubmit={e => e.preventDefault()}>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <input 
+                        type="tel"
+                        placeholder="Number"
+                        name="pno"
+                        value={formData.pno}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <input 
+                        type="date"
+                        placeholder="Date of Birth"
+                        name="dob"
+                        value={formData.dob}
+                        onChange={handleInputChange}
+                    />
+                </form>
+                <button type="reset" onClick={handleReset}>Reset</button>
+            </div>
+            <div className="personalDetails">
+                <h2>{formData.name}</h2>
+                <h3>{formData.email}</h3>
+                <h3>{formData.pno}</h3>
+                <h3>{formData.dob}</h3>
+            </div>
+        </div>
+    );
+}
+ 
+export default BasicDetails;
---
+++ react-cv/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import BasicDetails from './BasicDetails.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BasicDetails/>
+  </React.StrictMode>,
+)
