872c026923dceb9316367a002b492a4adca07550
Shriharsh <shriharsh.dev@gmail.com>
2023-12-05 12:51:33 +0530

    Created Experience component

---
+++ react-cv/src/Experience.jsx
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import './App.css';
+
+const Experience = () => {
+    const [expData, setExpData] = useState({
+        title:'',
+        desc:'',
+    })
+    const handleExpChange = (e) => {
+        const {name, value} = e.target;
+        setExpData({
+            ...expData,
+            [name]:value
+        })
+    }
+    const handleReset = () => {
+        setExpData({
+            title:'',
+            desc:''
+        })
+    }
+    return (
+        <div className="expDetails">
+            <div className="expDetailsForm">
+                <h1>Experience Details</h1>
+                <form onSubmit={e => e.preventDefault()}>
+                    <input 
+                        type="text"
+                        placeholder="Title"
+                        name="title"
+                        value={expData.title}
+                        onChange={handleExpChange}
+                    />
+                    <br />
+                    <textarea
+                        placeholder="Description"
+                        name="desc"
+                        value={expData.desc}
+                        onChange={handleExpChange}
+                        rows={10}
+                        cols={60}
+                    />
+                    <br />
+                    <button type="reset" onClick={handleReset}>Reset</button>
+                </form>
+            </div>
+            <div className="expDetails">
+                <h2>{expData.title}</h2>
+                <h4>{expData.desc}</h4>
+            </div>
+        </div>
+    );
+}
+ 
+export default Experience;
