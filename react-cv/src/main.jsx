26348ce5ed5a477722013d7abefd70b7be7f78dc
Shriharsh <shriharsh.dev@gmail.com>
2023-12-07 18:52:07 +0530

    Added css file route and heading

--- react-cv/src/main.jsx
+++ react-cv/src/main.jsx
@@ -6,6 +6,7 @@ import Experience from './Experience.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1 className="mainHeading">CV Builder</h1>
     <BasicDetails/>
     <Experience/>
   </React.StrictMode>,
